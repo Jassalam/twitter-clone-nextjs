c146e7dcd3d611204aa1150334819716de725108
JASVEER KAUR <jasveerk1980@gmail.com>
2023-04-20 12:34:12 +0200

    first commit with settings

---
+++ pages/components/NewTweet.js
@@ -0,0 +1,12 @@
+import { useSession } from 'next-auth/react'
+
+export default function NewTweet() {
+  const { data: session } = useSession()
+
+	//don't display if we're not logged in
+	if (!session) return null
+
+  return (
+    <p>form placeholder!</p>
+  )
+}
---
+++ pages/home.js
@@ -0,0 +1,16 @@
+import { useSession } from 'next-auth/react'
+import NewTweet from './components/NewTweet'
+
+export default function Home(){
+
+    const { data: session, status} = useSession()
+
+    if(status === 'loading'){
+        return <p>...</p>
+    }
+    return(
+        <div>
+            {session ? <NewTweet /> :<p>You are not logged in</p>}
+        </div>
+    )
+}
